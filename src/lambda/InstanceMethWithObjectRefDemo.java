7f6b5263ce49e69fdb4d6496b4b58458c0ec30c7
Rinaldo Santana <rinaldo.rms@gmail.com>
2025-03-06 22:21:18 -0300

    Add it class InstanceMethWithObjectRefDemo

---
+++ src/lambda/InstanceMethWithObjectRefDemo.java
@@ -0,0 +1,15 @@
+package lambda;
+
+/****
+ * Use an instance method reference with different objects.
+ * @param args
+ */
+
+public class InstanceMethWithObjectRefDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

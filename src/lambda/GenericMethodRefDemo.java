7bfa1e2247963e7b7de95c3b6b057fe23b592afd
Rinaldo Santana <rinaldo.rms@gmail.com>
2025-03-08 23:17:08 -0300

    Demostrate a method referene to a generic method declared inside a non-generic class

---
+++ src/lambda/GenericMethodRefDemo.java
@@ -0,0 +1,56 @@
+package lambda;
+
+
+/*
+ * 
+ * Demonstrate a method reference to a generic method declared inside a non-generic class.
+ * 
+ */
+
+
+
+interface MyFunc4<T> {
+	int func(T[] vals, T v);
+}
+
+
+
+class MyArrayOps{
+	static <T> int  countMatching(T[] vals, T v) {
+		int count = 0;
+		
+		for (int i = 0; i < vals.length; i++) {
+			if(vals[i] == v) count++;
+		}
+		
+		return count;
+	}
+}
+
+public class GenericMethodRefDemo {
+	
+	static <T> int myOp(MyFunc4<T> f, T[] vals, T v) {
+		return f.func(vals, v);
+	}
+
+	public static void main(String[] args) {
+
+		Integer[] vals = {1, 2, 3, 4, 2, 3, 4, 4, 5};
+		String[] strs = {"One", "Two", "Three", "Two"};
+		int count;
+		
+		count = myOp(MyArrayOps::<Integer>countMatching, vals, 4);
+		System.out.println("vals contains " + count + " 4s");
+		
+		count = myOp(MyArrayOps::<String>countMatching, strs, "Two");
+		System.out.println("vals contains " + count + " Twos");
+	}
+
+	
+	/*
+	 * Output:
+	 * vals contains 3 4s
+	 * vals contains 2 Twos
+	 * 
+	 */
+}
